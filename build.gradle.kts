9c6e2d7b207e88e01e39360a2645e9ecdf6eefb3
diliuskh <diliuskh@gmail.com>
2021-05-25 18:18:59 +0300

    updated dependencies for kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    val kotlinVersion = "1.5.0"
+    val kotlinVersion = "1.5.10"
     java
     kotlin("jvm") version kotlinVersion
 }
@@ -11,7 +11,8 @@ repositories {
     mavenCentral()
 }
 
-val kotlinVersion = "1.5.0"
+val kotlinVersion = "1.5.10"
+val coroutinesVersion = "1.5.0"
 val junitVersion = "5.8.0-M1"
 val mockitoVersion = "3.10.0"
 val mockitoKotlinVersion = "3.2.0"
@@ -19,6 +20,10 @@ val mockitoKotlinVersion = "3.2.0"
 dependencies {
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib-jdk8"))
+
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", coroutinesVersion)
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm", coroutinesVersion)
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-jdk8", coroutinesVersion)
 
     testImplementation(platform("org.junit:junit-bom:$junitVersion"))
     testImplementation("org.junit.jupiter", "junit-jupiter")
@@ -32,6 +37,7 @@ dependencies {
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 tasks {
     compileKotlin {
