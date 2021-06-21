77b9011ce7a269f73d9ac0f5b83035a0e0248e2e
diliuskh <diliuskh@gmail.com>
2021-06-21 15:25:10 +0300

    add gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 val kotlinVersion = "1.5.10"
 val coroutinesVersion = "1.5.0"
 val junitVersion = "5.8.0-M1"
-val mockitoVersion = "3.10.0"
+val mockitoVersion = "3.11.0"
 val mockitoKotlinVersion = "3.2.0"
 
 dependencies {
@@ -22,34 +22,35 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", coroutinesVersion)
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm", coroutinesVersion)
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-jdk8", coroutinesVersion)
 
     testImplementation(platform("org.junit:junit-bom:$junitVersion"))
     testImplementation("org.junit.jupiter", "junit-jupiter")
     testImplementation("org.junit.jupiter", "junit-jupiter-params")
     testImplementation("org.jetbrains.kotlin", "kotlin-test-junit5", kotlinVersion)
-    testImplementation("org.jetbrains.kotlin", "kotlin-test", kotlinVersion)
     testImplementation("org.mockito", "mockito-core", mockitoVersion)
     testImplementation("org.mockito", "mockito-junit-jupiter", mockitoVersion)
     testImplementation("org.mockito.kotlin", "mockito-kotlin", mockitoKotlinVersion)
 }
+tasks.test {
+    useJUnitPlatform()
+}
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_15
+    targetCompatibility = JavaVersion.VERSION_15
 }
 tasks {
     compileKotlin {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "11"
+            jvmTarget = "15"
         }
     }
     compileTestKotlin {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "11"
+            jvmTarget = "15"
         }
     }
 }
---
+++ src/main/kotlin/net/dilius/daily/coding/problem/y2020/m09/One.kt
@@ -0,0 +1,10 @@
+package net.dilius.daily.coding.problem.y2020.m09
+
+import net.dilius.daily.coding.problem.Problem
+
+class ProblemOne : Problem {
+    override fun solve() {
+        TODO("Not yet implemented")
+    }
+
+}
