7f553c6172298044eea2eb6edeae10105c4326b1
Dilius <diliuskh@gmail.com>
2023-03-12 18:45:58 +0100

    003 part 1

---
+++ src/test/kotlin/net/dilius/daily/coding/problem/n00x/BinaryTreeSerializerTest.kt
@@ -0,0 +1,12 @@
+package net.dilius.daily.coding.problem.n00x
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class BinaryTreeSerializerTest {
+
+    @Test
+    fun solve() {
+    }
+}
