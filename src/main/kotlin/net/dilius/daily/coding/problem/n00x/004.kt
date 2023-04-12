b54842c0b13d229b9893c538a75e6f7fadcaf05c
Dilius <diliuskh@gmail.com>
2023-04-12 23:50:58 +0200

    004

---
+++ src/main/kotlin/net/dilius/daily/coding/problem/n00x/004.kt
@@ -0,0 +1,2 @@
+package net.dilius.daily.coding.problem.n00x
+
---
+++ src/test/kotlin/net/dilius/daily/coding/problem/n00x/LowestPositiveNonExistingIntTest.kt
@@ -0,0 +1,5 @@
+package net.dilius.daily.coding.problem.n00x
+
+import org.junit.jupiter.api.Assertions.*
+
+class LowestPositiveNonExistingIntTest
